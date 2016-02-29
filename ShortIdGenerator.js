fa1a37c7ca1cc415e5b6ad5d61d874cc2802564d
vanita5 <mail@vanit.as>
2016-02-29 09:45:54 +0100

    correctly handle IDs (respect MAX id), upsert into cache (insert if not present, else update)

--- ShortIdGenerator.js
+++ ShortIdGenerator.js
@@ -3,6 +3,7 @@ module.exports = {
     MAX: 1295,
     current: 360,
     generate: function() {
+        if (this.current >= this.MAX) this.current = this.MIN;
         var _id = this.current.toString(36);
         this.current++;
         return _id;
