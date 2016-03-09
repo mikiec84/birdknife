a67e8e5c7d2e52dbd229582f587b207f5c80a603
vanita5 <mail@vanit.as>
2016-03-09 15:00:55 +0100

    implement preferences

---
+++ libs/birdknife-preferences.js
@@ -0,0 +1,51 @@
+var nconf = require('nconf'),
+    fs = require('fs'),
+    path = require('path');
+
+module.exports = {
+    originalPath: path.join(path.dirname(require.main.filename), 'config.json'),
+    configPath: path.join(process.env[(process.platform == 'win32' ? 'USERPROFILE' : 'HOME')], '.birdknife.json'),
+
+    init: function() {
+        if (this.isAccessible()) {
+            var local = require(this.originalPath);
+            var user = require(this.configPath);
+
+            if (local.version !== user.version) this.copyUserDir();
+        } else {
+            this.copyUserDir();
+        }
+        this.load();
+    },
+
+    get: function(key) {
+        return nconf.get(key);
+    },
+
+    isAccessible: function() {
+        try {
+            fs.accessSync(this.configPath, fs.F_OK);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    },
+
+    copyUserDir: function() {
+        try {
+            fs.writeFileSync(this.configPath,
+                fs.readFileSync(this.originalPath)
+            );
+        } catch (e) {
+            console.log(e);
+            this.configPath = this.originalPath;
+        }
+    },
+
+    load: function() {
+        const self = this;
+        nconf.argv()
+            .env()
+            .file({ file: self.configPath });
+    }
+};
